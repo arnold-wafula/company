ba59e774dc4ac89a71da465d490a16474d1f94ab
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-05-30 15:41:31 +0300

    Added PostController API routes

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Post\PostController;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::get('/get', [PostController::class, 'index']);
+Route::post('/post', [PostController::class, 'store']);
+Route::delete('/delete/{id}', [PostController::class, 'destroy']);
