97343e743f0ef44e184288c02be2d4baea7b5a24
arnold.wafula0@gmail.com <arnold.wafula0@gmail.com>
2024-05-31 12:27:57 +0300

    Added query to insert data

---
+++ database/seeders/PostSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Str;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class PostSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('posts')->insert([
+            'title' => Str::random(10),
+            'content' => Str::random(500)
+        ]);
+    }
+}
